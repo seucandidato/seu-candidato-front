5b8cbf62eed8d82c58765b8d91e2e4cd9215c1cb
Seu Candidato <repositorio@seucandidato.com>
2024-05-31 18:05:15 -0300

    seucandidato/seu-candidato-front#13 - correção botão login da tela de confirmação de usuário cadastrado

--- src/app/pages/register-success/register-success.component.ts
+++ src/app/pages/register-success/register-success.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-success',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './register-success.component.scss'
 })
 export class RegisterSuccessComponent {
+  
+  constructor(private router: Router) {}
 
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
